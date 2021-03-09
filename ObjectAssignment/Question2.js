29c4b1693283b5ec0123ff2c7753a2aac2291763
Selamawit123 <79427991+Selamawit123@users.noreply.github.com>
2021-03-08 21:34:09 -0600

    Update Question2.js

--- ObjectAssignment/Question2.js
+++ ObjectAssignment/Question2.js
@@ -1,13 +1,5 @@
 /*Write the function countProperties(obj) which returns number of properties of an object.*/
-//function countProperties()
-// const car = {
-//     color: 'Blue',
-//     brand: 'Ford',
-//     model: 'Fiesta'
-//   }
-  
-//   Object.keys(car).length
-
+ 
 let user={name:"selam", age: 10, isStudent: true};
 let count=0;
 for(let prop in user){
